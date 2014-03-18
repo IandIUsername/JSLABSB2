7f802951c31d201efd3824677ec25780e6c45b42
Scott <z89quark@gmail.com>
2014-03-18 05:13:06 +0000

    tagging feature complete, work on destroy

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,6 +1,12 @@
 Blogger::Application.routes.draw do
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+  
+
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
